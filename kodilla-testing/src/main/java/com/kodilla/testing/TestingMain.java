527f8d702db870411f9429f9f74e906f8897aa91
macmich981 <53463071+macmich981@users.noreply.github.com>
2019-08-08 10:08:44 +0200

    Task 2 from module 2 (#1)

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
@@ -12,6 +13,20 @@ public class TestingMain {
             System.out.println("test OK");
         } else {
             System.out.println("Error!");
+        }
+
+        int a = 10, b = 5;
+        Calculator calculator = new Calculator();
+        if (calculator.add(a, b) == a + b) {
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Error");
+        }
+
+        if (calculator.substract(a, b) == a - b) {
+            System.out.println("Test ok");
+        } else {
+            System.out.println("Error");
         }
     }
 }
