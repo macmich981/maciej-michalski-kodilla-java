80af4e450f881171388e318a2d946f6fa3d83578
macmich981 <macmic@onet.eu>
2019-09-28 20:38:05 +0200

    MoviesStoreTitles

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MoviesTitlesRunner.java
@@ -0,0 +1,15 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class MoviesTitlesRunner {
+    public static void main(String args[]) {
+        MovieStore movieStore = new MovieStore();
+
+        String moviesTitles = movieStore.getMovies().values().stream()
+                .flatMap(val -> val.stream())
+                .collect(Collectors.joining("!"));
+
+        System.out.print(moviesTitles);
+    }
+}
