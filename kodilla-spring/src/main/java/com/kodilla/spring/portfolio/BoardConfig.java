6369205a317cfc0f6f51225ece7b108555e0bf0b
macmich981 <macmic@onet.eu>
2019-10-16 19:37:54 +0200

    Removed fields from BoardConfig class and introduced methods

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,46 +1,28 @@
 package com.kodilla.spring.portfolio;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BoardConfig {
-    @Autowired
-    @Qualifier("toDoList")
-    TaskList toDoList;
-
-    @Autowired
-    @Qualifier("inProgressList")
-    TaskList inProgressList;
-
-    @Autowired
-    @Qualifier("doneList")
-    TaskList doneList;
 
     @Bean
     public Board getBoard() {
-        return new Board(toDoList, inProgressList, doneList);
+        return new Board(toDoList(), inProgressList(), doneList());
     }
 
-    @Bean(name = "toDoList")
-    @Scope("prototype")
-    public TaskList getToDoList() {
+    @Bean
+    public TaskList toDoList() {
         return new TaskList();
     }
 
-    @Bean(name = "inProgressList")
-    @Scope("prototype")
-    public TaskList getInProgressList() {
+    @Bean
+    public TaskList inProgressList() {
         return new TaskList();
     }
 
-    @Bean(name = "doneList")
-    @Scope("prototype")
-    public TaskList getDoneList() {
+    @Bean
+    public TaskList doneList() {
         return new TaskList();
     }
 }
