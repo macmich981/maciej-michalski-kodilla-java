aab6f1c9bc7de9deb541e88847397fd3d07ce76b
macmich981 <macmic@onet.eu>
2019-11-03 23:31:32 +0100

    Test.17.2 small corrects

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
@@ -3,7 +3,6 @@ package com.kodilla.hibernate.task.dao;
 import com.kodilla.hibernate.task.Task;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
