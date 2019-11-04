4110452219eb533658218f7db7ddae505e0c7e4b
macmich981 <53463071+macmich981@users.noreply.github.com>
2019-11-04 07:56:34 +0100

    Test.17.2 - Tasklists entity created (#30)
    * Test.17.2 - Tasklists entity created
    
    * Test.17.2 small corrects
    
    * Test.17.2 small corrects

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskListDaoTestSuite {
+    @Autowired
+    private TaskListDao taskListDao;
+    private static final String LIST_NAME = "List to do";
+    private static final String DESCRIPTION = "Tasks to do";
+
+    @Test
+    public void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList(LIST_NAME, DESCRIPTION);
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+        //When
+        List<TaskList> readTaskList = taskListDao.findByListName(listName);
+        //Then
+        Assert.assertEquals("List to do", readTaskList.get(0).getListName());
+        Assert.assertEquals(1, readTaskList.size());
+        //CleanUp
+        int id = readTaskList.get(0).getId();
+        taskListDao.deleteById(id);
+    }
+}
