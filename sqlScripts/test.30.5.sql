0931a0c90c22c797354d29e98911983d0405cc1b
macmich981 <macmic@onet.eu>
2020-02-13 00:13:05 +0100

    Test 30.5 - corrected code with cleaning

--- sqlScripts/test.30.5.sql
+++ sqlScripts/test.30.5.sql
@@ -4,44 +4,27 @@ CREATE TABLE stats
 (
 	stat_id 	INT(11) AUTO_INCREMENT PRIMARY KEY,
 	stat_date 	DATETIME NOT NULL,
-	state 		VARCHAR(20) NOT NULL,
+	stat 		VARCHAR(20) NOT NULL,
 	value INT(11) NOT NULL
 );
-
-SELECT * FROM stats;
 
 CREATE VIEW bestsellers_count AS
 SELECT COUNT(*) AS bestsellers
 FROM books
 WHERE bestseller = 1;
 
-DROP VIEW bestsellers_count;
-
-COMMIT;
-
-INSERT INTO STATS (state_date, stat, value)
-VALUES (CURRENT_TIMESTAMP, "Bestsellers", (SELECT * FROM bestsellers_count));
-
-SELECT * FROM kodilla_course.bestsellers_count;
-
-delimiter $$
+DELIMITER $$
 
 CREATE EVENT update_bestsellers_count
 	ON SCHEDULE EVERY 1 MINUTE
     DO 
 		BEGIN
 			CALL UpdateBestsellers();
-			INSERT INTO stats (state_date, stat, value) 
+			INSERT INTO stats (stat_date, stat, value) 
             VALUES (CURRENT_TIMESTAMP, "Bestsellers", (SELECT * FROM bestsellers_count));
             COMMIT;
 		END $$
-        
-delimiter ;
+
+DELIMITER ;
 
 DROP EVENT update_bestsellers_count;
-
-COMMIT;
-
-SELECT * FROM kodilla_course.stats;
-
-SELECT * FROM kodilla_course.bestsellers_count;
